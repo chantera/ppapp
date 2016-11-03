f8f832f9729f435f764cbf6fc83ed5736d7322f9
Hiroki Teranishi <teranishihiroki@gmail.com>
2016-11-03 21:09:09 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# PPAPP
+
+Pen-Pineapple-Apple-Pen for Python
+[PPAP](https://www.youtube.com/watch?v=0E00Zuayv9Q)
+
+
+## Installation
+
+```sh
+$ git https://github.com/chantera/ppapp
+```
+
+## Usage
+
+```sh
+$ python ppapp/ppap.py [iteration]
+```
+
+----
+&copy; Copyright 2016 Teranishi Hiroki
---
+++ ppap.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+class PikoTaro:
+
+    def uh(self, objects):
+        assert len(objects) == 2
+        stack = []
+        for i, o in enumerate(objects[::-1]):
+            if type(o) == list:
+                stack += o[::-1] if i == 0 else o
+            elif type(o) == str and '-' in o:
+                o = o.split('-')
+                stack += o[::-1] if i == 0 else o
+            else:
+                stack.append(o)
+        print('Uh!')
+        return '-'.join(stack)
+
+
+def ppap(iteration):
+    pikotaro = PikoTaro()
+    ap = pikotaro.uh([s.split()[-1] for s in ("I have a Pen", "I have a Apple")])
+    print(ap)
+    pp = pikotaro.uh([s.split()[-1] for s in ("I have a Pen", "I have a Pineapple")])
+    print(pp)
+    ppap = pikotaro.uh((ap, pp))
+    print(ppap)
+
+    if iteration > 0:
+        print('***')
+
+        for i in range(iteration):
+            ppap = pikotaro.uh((ppap, ppap))
+            print(ppap)
+
+
+if __name__ == "__main__":
+    ppap(iteration=(int(sys.argv[1]) if len(sys.argv) > 1 else 5))
